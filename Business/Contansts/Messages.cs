54ad35d560f692d7fbc29eecc7ce2407d24e39c4
rıfat işler <isler.rifat@gmail.com>
2023-05-01 22:21:42 +0300

    01052023

---
+++ Business/Contansts/Messages.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.Contansts
+{
+    public static class Messages
+    {
+        public static string ProductDeleted = "Ürün Başarıyla Silindi";
+        public static string ProductAdded = "Ürün Başarıyla Eklendi";
+        public static string ProductUpdated = "Ürün Başarıyla Güncellendi";
+    }
+}
---
+++ WebAPIM/Controllers/ProductsController.cs
@@ -0,0 +1,94 @@
+﻿using Business.Abstract;
+using Entities.Concrete;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAPIM.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        private IProductService _productService;
+
+        public ProductsController(IProductService productService)
+        {
+            _productService = _productService;
+        }
+
+        [HttpGet(template: "getall")]
+        public IActionResult GetList()
+        {
+            var result = _productService.GetList();
+            if (result.Success)
+            {
+                return Ok(result.Data);
+            }
+            return BadRequest(result.Message);
+        }
+
+        [HttpGet(template: "getlistbycategory")]
+        public IActionResult GetListByCategory(int categoryId)
+        {
+            var result = _productService.GetListByCategory(categoryId);
+            if (result.Success)
+            {
+                return Ok(result.Data);
+            }
+
+            return BadRequest(result.Message);
+        }
+
+        [HttpGet(template: "getbyid")]
+        public IActionResult GetById(int productId)
+        {
+            var result = _productService.GetById(productId);
+            if (result.Success)
+            {
+                return Ok(result.Data);
+            }
+
+            return BadRequest(result.Message);
+        }
+
+        [HttpGet(template: "add")]
+        public IActionResult Add(Product product)
+        {
+            var result = _productService.Add(product);
+            if (result.Success)
+            {
+                return Ok(result.Message);
+            }
+
+            return BadRequest(result.Message);
+        }
+
+        [HttpGet(template: "update")]
+        public IActionResult Update(Product product)
+        {
+            var result = _productService.Update(product);
+            if (result.Success)
+            {
+                return Ok(result.Message);
+            }
+
+            return BadRequest(result.Message);
+        }
+
+        [HttpGet(template: "delete")]
+        public IActionResult Delete(Product product)
+        {
+            var result = _productService.Delete(product);
+            if (result.Success)
+            {
+                return Ok(result.Message);
+            }
+
+            return BadRequest(result.Message);
+        }
+    }
+}
