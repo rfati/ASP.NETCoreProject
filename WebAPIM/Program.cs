7680f67475069c4827d58a283c7b1c13eea7500b
rfati <isler.rifat@gmail.com>
2023-05-02 14:35:31 +0300

    02052023

--- WebAPIM/Program.cs
+++ WebAPIM/Program.cs
@@ -1,3 +1,5 @@
+using Autofac;
+using Autofac.Extensions.DependencyInjection;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
@@ -6,6 +8,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Business.DependencyResolvers.Autofac;
+
 
 namespace WebAPIM
 {
@@ -18,6 +22,11 @@ namespace WebAPIM
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
+            .UseServiceProviderFactory(new AutofacServiceProviderFactory())
+            .ConfigureContainer<ContainerBuilder>(builder =>
+            {
+                builder.RegisterModule(new AutofacBusinesssModule());
+            })
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
